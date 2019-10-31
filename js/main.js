fb45fc63d001b298842e260ef86c6ae18954a29c
Devan Terry <devanterry@Devans-MacBook-Pro.local>
2019-10-31 16:40:12 -0700

    added logic

--- js/main.js
+++ js/main.js
@@ -36,7 +36,11 @@ function init() {
         [0, 0, 0],
         [0, 0, 0],
     ]
-
+    for (let i = 0; i < cells.length; i++){
+        cells[i].className = 'cell';
+        //HTMLElement.className overwrites the entire current classname. HTMLElement.classList.add will add onto the current class names and HTMLEElement.classList.remove will only remove the specified class name if it exists.
+    };
+    message.textContent = 'Good luck!';
     render();
 };
 
@@ -70,67 +74,96 @@ function render(){
 };
 
 function handleClick(evt) {
-
+    if (evt.target.className === 'disabled' || winner !== null){
+        return;
+    }
     if (evt.target.className === 'cell'){
         let evtId = evt.target.id;
-        let column = evtId[1];
-        let row = evtId[3];
-        cells.forEach(function (e) {
-            if (evtId === e.id) {
+        let column = parseInt(evtId[1]);
+        let row = parseInt(evtId[3]);
+        // cells.forEach(function (e) {
+        //     if (evtId === e.id) {
                 board[column][row] = turn;
-            }
+        //     }
             attempts++;
-        });
-        evt.className = 'disabled';
+        // });
+        evt.target.className = 'disabled';
     }
+    gameOver();
     console.log(board);
     turn = switchTurn();
     render();
 }
 
 function getRandomNum() {
+    //Math.random() returns between 0-0.9999999. If multiplied by 2, returns between 0-1.99999999. Math.floor will round down, so we will either get between 0 and 1. 
     let random = Math.floor(Math.random() * 2);
+    //random ? 1 : -1 translates to ==> if random is true, then return 1 else return -1. 1 is considered 'true' and 0 is considered 'false'
     return random ? 1 : -1;
 };
 
 
 function checkRowWinner() {
-  for(let row = 0; row < BOARD_SIZE; row++);
+  for(let row = 0; row < BOARD_SIZE; row++){
   let sum = 0;
-    for(let column = 0; column < BOARD_SIZE; column++);
-      sum += boardPosition(row, column);
+    for(let column = 0; column < BOARD_SIZE; column++){
+      sum += board[row][column];
+    };
     if (Math.abs(sum) === BOARD_SIZE){
-        message = player + "Wins!";
+        message.textContent = turn + " Wins!";
+        winner = turn;
         console.log(message);
     }
+};
 };
 
 function checkColumnWinner(){
-  for(let column = 0; column < BOARD_SIZE; column++);
+  for(let column = 0; column < BOARD_SIZE; column++){
   let sum = 0;
-    for(let row =0; row < BOARD_SIZE; row++);
-      sum += boardPosition(row, column);
-    if (Math.abs(sum) === BOARD_SIZE){
-        message = player + "Wins!";
+    for(let row =0; row < BOARD_SIZE; row++){
+      sum += board[row][column];
+  };
+  if (Math.abs(sum) === BOARD_SIZE){
+        message.textContent = turn + " Wins!";
+        winner = turn;
         console.log(message);
     }
 };
+};
  
 function checkDiagonals(){
-    let downSum = 0;
-    let upSum = 0;
-
-    for(let column = 0, row = 0; column < BOARD_SIZE, row < BOARD_SIZE; column++, row++){
-        let sum = 0;
-
+    let diagonal1 = 0;
+    let diagonal2 = 0;
+    for(let i = 0; i < 3; i++){
+        diagonal1 += board[i][i];
+        diagonal2 += board[i][2-i];
+    };
+    if (Math.abs(diagonal1) === 3 || Math.abs(diagonal2) === 3){
+        message.textContent = turn + " Wins!";
+        winner = turn;
+        console.log(message);
     }
+    // let downSum = 0;
+    // let upSum = 0;
+    // for(let column = 0, row = 0; column < BOARD_SIZE, row < BOARD_SIZE; column++, row++){
+    //     let sum = 0;
+        
+    // }
 };
 
 function checkTie(){
-if(attempts === ATTEMPT_LIMIT){
-    message = "It's a tie!";
+    let counter = 0;
+for(i = 0; i < BOARD_SIZE; i++){
+    for(j = 0; j < BOARD_SIZE; j++){
+        counter += Math.abs(board[i][j]);
+        };
+    };
+    if (counter === 9){
+        return message.textContent = "It's a tie!";
+    };
 };
-}
+
+// };
 
 
 function gameOver(){
